58501036aff8e947a28a9ba5efbd46d73356c54c
pejman mowloudi <pejmanmowloudi@gmail.com>
2019-09-15 11:47:52 +0430

    Change error insert AppicationApi

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -6,16 +6,4 @@ class AccessTokensController < ApplicationController
 		authenticator = UserAuthenticator.new(params[:code])
 		authenticator.perform
   end
-
-  private 
-
-  def authentication_error
-  			error = {
-			      "status" => "401",
-			      "source" => {"pointer"=>  "/code"},
-			      "title" => "Authentication code is invalid",
-			      "detail" => "You must provide valid code in order to exchange it for token."
-			    }	
-		render json: {"errors": [error]}, status: 401
-  end
 end
