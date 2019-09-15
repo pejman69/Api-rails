a562a0daee62ef4ed947311d54449bde017de29e
pejman mowloudi <pejmanmowloudi@gmail.com>
2019-09-15 14:42:12 +0430

    Create Serializer and change generate_token

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -5,6 +5,6 @@ class AccessTokensController < ApplicationController
 	def create
 		authenticator = UserAuthenticator.new(params[:code])
 		authenticator.perform
-		render json: {}, status: :created
+		render json: authenticator.access_token, status: :created
   end
 end
