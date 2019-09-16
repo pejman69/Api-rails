1ca188e83291cc12cae78ccc8331e20fedb6fbf5
pejman mowloudi <pejmanmowloudi@gmail.com>
2019-09-16 13:38:12 +0430

    Generate Comment and rake routes  and change codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
+
   post 'login', to: 'access_tokens#create'
   delete 'logout', to: 'access_tokens#destroy'
-  resources :articles
+  resources :articles do
+  	  resources :comments, only: [:index, :create]
+  	end
   end
