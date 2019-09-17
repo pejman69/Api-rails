ca4fe33995dbc3934d216204a245fb76eb1a7ad3
pejman mowloudi <pejmanmowloudi@gmail.com>
2019-09-17 09:18:47 +0430

    shared oauth and standard

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(
+  {
+      login: 'jsmith', name: 'John Smith', provider: 'github' }
+)
 Article.create([{
 title: 'Article title 1', content: 'Article content 1', slug: 'article-title-1'},
 {
